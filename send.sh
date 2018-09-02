9148c9bb472335447301ff644de92990063a9385
pktrigg <pktrigg@gmail.com>
2018-09-02 15:07:25 +0800

    error checking when finding IP

--- send.sh
+++ send.sh
@@ -2,3 +2,5 @@ for (( ; ; ))
 do
 	date | nc -uw 1 192.168.15.80 5021
 done
+sdf
+dfddd
--- src/udpserver.py
+++ src/udpserver.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 #name:		  UDPServer
 #created:	   August 2018
 #by:			paul.kennedy@guardiangeomatics.com
@@ -7,22 +8,54 @@
 # See readme.md for more details
 
 import socket
+import socket
+import fcntl
+import struct
+
+def get_ip_address(ifname):
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	try:
+		return socket.inet_ntoa(fcntl.ioctl(
+			s.fileno(),
+			0x8915,  # SIOCGIFADDR
+			struct.pack('256s', bytes(ifname[:15], 'utf-8'))
+			)[20:24])
+	except:
+		return ('localhost')
 
 # Here we define the UDP IP address as well as the port number that we have 
 # already defined in the client python script.
 
-UDP_IP_ADDRESS = "192.168.15.87"
-UDP_PORT_NO = 5019
-# Once we''ve imported the socket module and declared our ip address and port number we can create another socket which will look exactly like the socket we constructed in our client program.
+def main():
+	'''UDP SERVER receives data from a client and does something with it (typically capture to disc)'''
+	UDP_PORT_NO = 5019
+	print ("Listening on UDP Port: %d" %(UDP_PORT_NO))
+	eth0 = get_ip_address('eth0')
+	print ("Ethernet IP: %s will be used as priority" % (eth0))
+	if eth0 is 'localhost':
+		wlan0 = get_ip_address('wlan0')
+		UDP_IP_ADDRESS = wlan0
+		print ("WiFi IP:", wlan0)
+	else:	
+		UDP_IP_ADDRESS = eth0
 
-# declare our serverSocket upon which
-# we will be listening for UDP messages
-serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# One difference is that we will have to bind our declared IP address
-# and port number to our newly declared serverSock
-serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
-# And finally, once we've created our server socket, we need to write the code that will keep our script continuously listening to this socket until its termination. This takes form as a simple while loop, like so:
+	print ("Listening on IP:", UDP_IP_ADDRESS)
 
-while True:
-	data, addr = serverSock.recvfrom(1024)
-	print ("Message: ", data)
+	# declare our serverSocket upon which
+	# we will be listening for UDP messages
+	serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# One difference is that we will have to bind our declared IP address
+	# and port number to our newly declared serverSock
+	serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+	# And finally, once we've created our server socket, we need to write the code that will keep our script continuously listening to this socket until its termination. This takes form as a simple while loop, like so:
+
+	while True:
+		data, addr = serverSock.recvfrom(1024)
+		print ("Message: ", data)
+
+
+
+###############################################################################################
+if __name__ == "__main__":
+	main()
+	# exit()
